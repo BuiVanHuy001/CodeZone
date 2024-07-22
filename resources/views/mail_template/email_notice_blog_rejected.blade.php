692fd47324d23fd0be4cbcbc0f72f574a296a7b8
Bui Van Huy <huy423210063@caodangvietmy.edu.vn>
2024-07-22 22:17:05 +0700

    student update status

---
+++ resources/views/mail_template/email_notice_blog_rejected.blade.php
@@ -0,0 +1,194 @@
+<!doctype html>
+<html ⚡4email data-css-strict>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="x-apple-disable-message-reformatting">
+  <style amp4email-boilerplate>
+    body {
+      visibility: hidden
+    }
+  </style>
+
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+
+
+  <style amp-custom>
+    .u-row {
+      display: flex;
+      flex-wrap: nowrap;
+      margin-left: 0;
+      margin-right: 0;
+    }
+
+    .u-row .u-col {
+      position: relative;
+      width: 100%;
+      padding-right: 0;
+      padding-left: 0;
+    }
+
+
+    .u-row .u-col.u-col-100 {
+      flex: 0 0 100%;
+      max-width: 100%;
+    }
+
+
+    @media (max-width: 767px) {
+      .u-row:not(.no-stack) {
+        flex-wrap: wrap;
+      }
+
+      .u-row:not(.no-stack) .u-col {
+        flex: 0 0 100%;
+        max-width: 100%;
+      }
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+    }
+
+    table,
+    tr,
+    td {
+      vertical-align: top;
+      border-collapse: collapse;
+    }
+
+    p {
+      margin: 0;
+    }
+
+    .ie-container table,
+    .mso-container table {
+      table-layout: fixed;
+    }
+
+    * {
+      line-height: inherit;
+    }
+
+    table,
+    td {
+      color: #000000;
+    }
+  </style>
+
+
+</head>
+
+<body class="clean-body u_body" style="margin: 0;padding: 0;background-color: #f9f9f9;color: #000000">
+  <!--[if IE]><div class="ie-container"><![endif]-->
+  <!--[if mso]><div class="mso-container"><![endif]-->
+  <table style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #f9f9f9;width:100%" cellpadding="0" cellspacing="0">
+    <tbody>
+      <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse;vertical-align: top">
+          <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #f9f9f9;"><![endif]-->
+
+          <div style="padding: 0px;">
+            <div style="max-width: 600px;margin: 0 auto;background-color: #ffffff;">
+              <div class="u-row">
+
+                <div class="u-col u-col-100" style="display:flex;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                  <div style="width: 100%;padding:0px;">
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td style="overflow-wrap:break-word;word-break:break-word;padding:20px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                              <tr>
+                                <td style="padding-right: 0px;padding-left: 0px;" align="center">
+
+                                  <amp-img alt="Image" src="https://assets.unlayer.com/projects/238755/1719058543551-logo.png" width="1413" height="395" layout="intrinsic" style="width: 32%;max-width: 32%;">
+
+                                  </amp-img>
+                                </td>
+                              </tr>
+                            </table>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div style="padding: 0px;">
+            <div style="max-width: 600px;margin: 0 auto;background-color: #ffffff;">
+              <div class="u-row">
+
+                <div class="u-col u-col-100" style="display:flex;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                  <div style="width: 100%;padding:0px;">
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td style="overflow-wrap:break-word;word-break:break-word;padding:33px 55px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <div style="font-size: 14px; line-height: 160%; text-align: center; word-wrap: break-word;">
+                              <div>
+                                <div data-message-id="8fbcc6b7-7237-481d-8540-bb7197bb39cc" data-message-author-role="assistant" dir="auto">
+                                  <div>
+                                    <div>
+                                      <p style="line-height: 160%; text-align: left;">Chào <strong>{{ $blog->author->user->name }}</strong>,<br>Mong rằng bạn đang có một ngày tốt lành. CodeZone thông báo về tình trạng gần đây của bài blog mà bạn đã gửi lên nền tảng CodeZone.</p>
+                                      <p style="line-height: 160%; text-align: left;">Sau khi xem xét kỹ lưỡng, chúng tôi rất tiếc phải thông báo rằng bài viết của bạn có tiêu đề "<strong>{{ $blog->title }}</strong>" đã <strong>không được chấp nhận</strong> để xuất bản trên nền tảng của chúng tôi. Đội ngũ của chúng tôi đã thẩm định mọi đề xuất dựa trên các hướng dẫn nội dung để đảm bảo chất lượng cao nhất và sự phù hợp với đối tượng độc giả.</p>
+                                      <p style="line-height: 160%; text-align: left;">Dưới đây là vài lý do vì sao đề xuất của bạn có thể không đáp ứng tiêu chuẩn của chúng tôi:</p>
+                                      <ul style="text-align: left;">
+                                        <li style="line-height: 22.4px;">Chủ đề bài viết không phù hợp với sở thích của đối tượng độc giả của chúng tôi.</li>
+                                        <li style="line-height: 22.4px;">Nội dung không đạt tiêu chuẩn về sự rõ ràng, sâu sắc hoặc sự độc đáo.</li>
+                                        <li style="line-height: 22.4px;">Có các vấn đề kỹ thuật như định dạng hoặc khó đọc đã được nhận diện.</li>
+                                      </ul>
+                                      <p style="line-height: 160%; text-align: left;">Xin lưu ý rằng quyết định này không phản ánh chất lượng công việc của bạn mà là sự phù hợp với trọng tâm biên tập hiện tại của chúng tôi.</p>
+                                      <p style="line-height: 160%; text-align: left;">&nbsp;</p>
+                                      <p style="line-height: 160%; text-align: left;">Chúng tôi khuyến khích bạn xem xét lại các hướng dẫn của chúng tôi và cân nhắc gửi các bài viết trong tương lai phù hợp hơn với chủ đề và mong đợi của độc giả của nền tảng chúng tôi. Đóng góp của bạn rất quan trọng và chúng tôi rất biết ơn sự thông cảm của bạn trong vấn đề này.</p>
+                                      <p style="line-height: 160%; text-align: left;">&nbsp;</p>
+                                      <p style="line-height: 160%; text-align: left;">Nếu bạn có bất kỳ câu hỏi nào hoặc muốn nhận được phản hồi chi tiết hơn về quá trình đánh giá, xin vui lòng liên hệ với chúng tôi qua email: <a rel="noreferrer">support.codezone@gmail.com</a> hoặc hotline: 19001919.</p>
+                                      <p style="line-height: 160%; text-align: left;">Cảm ơn bạn đã quan tâm đến CodeZone. Chúng tôi rất mong có cơ hội hợp tác với bạn trong các đề xuất sắp tới.</p>
+                                      <p style="line-height: 160%; text-align: left;">&nbsp;</p>
+                                      <p style="line-height: 160%; text-align: left;">Trân trọng,</p>
+                                      <p style="line-height: 160%; text-align: left;">&nbsp;</p>
+                                      <p style="line-height: 160%; text-align: left;">CodeZone Team</p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                              <div>
+                                <div>
+                                  <div style="text-align: left;">&nbsp;</div>
+                                </div>
+                              </div>
+                            </div>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <!--[if mso]></div><![endif]-->
+  <!--[if IE]></div><![endif]-->
+</body>
+
+</html>
---
+++ resources/views/mail_template/email_notice_instructor_course_rejected.blade.php
@@ -0,0 +1,174 @@
+<!doctype html>
+<html ⚡4email data-css-strict>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="x-apple-disable-message-reformatting">
+  <style amp4email-boilerplate>
+    body {
+      visibility: hidden
+    }
+  </style>
+
+  <script async src="https://cdn.ampproject.org/v0.js"></script>
+
+
+  <style amp-custom>
+    .u-row {
+      display: flex;
+      flex-wrap: nowrap;
+      margin-left: 0;
+      margin-right: 0;
+    }
+
+    .u-row .u-col {
+      position: relative;
+      width: 100%;
+      padding-right: 0;
+      padding-left: 0;
+    }
+
+
+    .u-row .u-col.u-col-100 {
+      flex: 0 0 100%;
+      max-width: 100%;
+    }
+
+
+    @media (max-width: 767px) {
+      .u-row:not(.no-stack) {
+        flex-wrap: wrap;
+      }
+
+      .u-row:not(.no-stack) .u-col {
+        flex: 0 0 100%;
+        max-width: 100%;
+      }
+    }
+
+    body {
+      margin: 0;
+      padding: 0;
+    }
+
+    table,
+    tr,
+    td {
+      vertical-align: top;
+      border-collapse: collapse;
+    }
+
+    p {
+      margin: 0;
+    }
+
+    .ie-container table,
+    .mso-container table {
+      table-layout: fixed;
+    }
+
+    * {
+      line-height: inherit;
+    }
+
+    table,
+    td {
+      color: #000000;
+    }
+
+    #u_body a {
+      color: #0000ee;
+      text-decoration: underline;
+    }
+  </style>
+
+
+</head>
+
+<body class="clean-body u_body" style="margin: 0;padding: 0;background-color: #f9f9f9;color: #000000">
+  <!--[if IE]><div class="ie-container"><![endif]-->
+  <!--[if mso]><div class="mso-container"><![endif]-->
+  <table id="u_body" style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;vertical-align: top;min-width: 320px;Margin: 0 auto;background-color: #f9f9f9;width:100%" cellpadding="0" cellspacing="0">
+    <tbody>
+      <tr style="vertical-align: top">
+        <td style="word-break: break-word;border-collapse: collapse;vertical-align: top">
+          <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td align="center" style="background-color: #f9f9f9;"><![endif]-->
+          <div style="padding: 0px;">
+            <div style="max-width: 600px;margin: 0 auto;background-color: #ffffff;">
+              <div class="u-row">
+
+                <div class="u-col u-col-100" style="display:flex;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                  <div style="width: 100%;padding:0px;">
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td style="overflow-wrap:break-word;word-break:break-word;padding:20px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <table width="100%" cellpadding="0" cellspacing="0" border="0">
+                              <tr>
+                                <td style="padding-right: 0px;padding-left: 0px;" align="center">
+
+                                  <amp-img alt="Image" src="https://assets.unlayer.com/projects/238755/1719058543551-logo.png" width="1413" height="395" layout="intrinsic" style="width: 32%;max-width: 32%;">
+
+                                  </amp-img>
+                                </td>
+                              </tr>
+                            </table>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <div style="padding: 0px;">
+            <div style="max-width: 600px;margin: 0 auto;background-color: #ffffff;">
+              <div class="u-row">
+
+                <div class="u-col u-col-100" style="display:flex;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;">
+                  <div style="width: 100%;padding:0px;">
+
+                    <table style="font-family:'Cabin',sans-serif;" role="presentation" cellpadding="0" cellspacing="0" width="100%" border="0">
+                      <tbody>
+                        <tr>
+                          <td style="overflow-wrap:break-word;word-break:break-word;padding:33px 55px;font-family:'Cabin',sans-serif;" align="left">
+
+                            <div style="font-size: 14px; line-height: 160%; text-align: center; word-wrap: break-word;">
+                              <p style="line-height: 160%; text-align: left;">Chào {{ $course->author->user->name }},</p>
+                              <p style="line-height: 160%; text-align: left;">&nbsp;</p>
+                              <p style="line-height: 160%; text-align: left;">Chúng tôi muốn thông báo rằng khóa học "{{ $course->title }}" của bạn đã được xem xét bởi đội ngũ kiểm duyệt của CodeZone. Sau quá trình kiểm tra cẩn thận và cân nhắc, chúng tôi rất tiếc phải thông báo rằng khóa học của bạn không đạt tiêu chuẩn để được đăng tải lên nền tảng của chúng tôi vào thời điểm này.</p>
+                              <p style="line-height: 160%; text-align: left;">Chúng tôi hiểu rằng đây có thể là tin không mong đợi. Tuy nhiên, chúng tôi luôn sẵn lòng hỗ trợ bạn trong việc cải thiện khóa học để đáp ứng các tiêu chuẩn chất lượng của CodeZone.</p>
+                              <p style="line-height: 160%; text-align: left;">Nếu bạn có bất kỳ câu hỏi hoặc cần sự hỗ trợ thêm, xin đừng ngần ngại liên hệ với chúng tôi qua email: <a rel="noreferrer">support.codezone@gmail.com</a> hoặc hotline: 19001919. Chúng tôi mong muốn tiếp tục làm việc cùng bạn để mang lại những khóa học chất lượng cao cho cộng đồng học viên của CodeZone.</p>
+                              <p style="line-height: 160%; text-align: left;">Chân thành cảm ơn bạn đã hiểu và hợp tác.</p>
+                              <p style="line-height: 160%; text-align: left;">Trân trọng,<br>CodeZone</p>
+                            </div>
+
+                          </td>
+                        </tr>
+                      </tbody>
+                    </table>
+
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+
+          <!--[if (mso)|(IE)]></td></tr></table><![endif]-->
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <!--[if mso]></div><![endif]-->
+  <!--[if IE]></div><![endif]-->
+</body>
+
+</html>
