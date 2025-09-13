490820eb360259498ec5590ccd979c38da37dea8
Bui Van Huy <huy423210063@caodangvietmy.edu.vn>
2025-09-13 15:43:11 +0700

    refactor(organization-dashboard): add assignment and quiz components with modal integration

---
+++ app/Livewire/Client/Lesson/Components/PracticeTypes/Quiz.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Livewire\Client\Lesson\Components\PracticeTypes;
+
+use Livewire\Component;
+
+class Quiz extends Component {
+    public function render()
+    {
+        return view('livewire.client.lesson.components.practice-types.quiz');
+    }
+}
